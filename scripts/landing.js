501f85aec547ad3a60c0e03058de92199e5287ec
briansmont <briansmont830@gmail.com>
2016-07-05 14:22:05 -0400

    Phase2 Assignment8 - Refactoring repetitive code & new animations

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,5 @@
-var animatePoints = function() {
+//Code animates the options at the bottom of the index.html page
+/*var animatePoints = function() {
     var points = document.getElementsByClassName("point");
     var revealFirstPoint = function() {
         points[0].style.opacity = 1;
@@ -25,3 +26,20 @@ var animatePoints = function() {
     revealThirdPoint();
 };
                 
+animatePoints();
+*/
+//Refactored code below:
+var animatePoints = function() {
+    var points = document.getElementsByClassName("point");
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebKitTransform = "scaleX(1) translateY(0)";
+    };
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
+};
+
+
